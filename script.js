d3eac4cf253f8b4a5ab646a0c5faa209764ab33e
Nicko Buenviaje <74237653+FalsePatronus@users.noreply.github.com>
2023-07-17 13:51:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 // script.js
 
-// Function to display a welcome message
-function displayWelcomeMessage() {
-  var message = "Welcome to Holy Spirit Integrated School!";
-  alert(message);
-}
-
 // Function to change the background color of the header
 function changeHeaderColor() {
   var header = document.querySelector(".header");
-  header.style.backgroundColor = "pink";
+  header.style.backgroundColor = "violet";
 }
 
 // Function to handle a button click event
