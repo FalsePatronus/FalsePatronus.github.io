bb5d84e71495ef1c772e82598811f6dea2e3241d
Nicko Buenviaje <74237653+FalsePatronus@users.noreply.github.com>
2023-07-17 14:00:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ function displayWelcomeMessage() {
 
 // Call the function
 displayWelcomeMessage(); // Display a welcome message when the page loads
+
+// script.js
+
+// Function to handle the click event of the "here" hyperlinks
+function handleHyperlinkClick(event) {
+  event.preventDefault(); // Prevent the default hyperlink behavior
+
+  var url = this.getAttribute("href"); // Get the URL from the hyperlink's href attribute
+  var subpage = window.open(url, "_blank", "width=800,height=600,top=100,left=100,resizable=yes"); // Open the subpage in a new floating window
+  subpage.focus(); // Focus on the new window
+}
+
+// Function to attach the click event handler to the "here" hyperlinks
+function attachHyperlinkClickEvent() {
+  var hyperlinks = document.querySelectorAll(".box a");
+  hyperlinks.forEach(function(hyperlink) {
+    hyperlink.addEventListener("click", handleHyperlinkClick);
+  });
+}
+
+// Call the function to attach the click event handler to the hyperlinks
+attachHyperlinkClickEvent();
+
