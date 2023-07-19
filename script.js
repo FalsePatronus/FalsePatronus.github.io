a79f61bb705624fd47dd415ae2df3103fb5cab4f
Nicko Buenviaje <74237653+FalsePatronus@users.noreply.github.com>
2023-07-19 10:51:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,16 @@
-// script.js
+// Smooth scrolling for anchor links
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
 
-// Function to display a welcome message when the page loads
-function displayWelcomeMessage() {
-  var welcomeMessage = "Welcome to Holy Spirit Integrated School!";
-  var welcomeElement = document.createElement("h2");
-  welcomeElement.textContent = welcomeMessage;
-  
-  var contentContainer = document.querySelector(".content");
-  contentContainer.prepend(welcomeElement);
-}
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
 
-// Call the function
-displayWelcomeMessage(); // Display a welcome message when the page loads
+// Activate Bootstrap carousel
+$('#testimonial-carousel').carousel();
 
-// script.js
-
-// Function to handle the click event of the "here" hyperlinks
-function handleHyperlinkClick(event) {
-  event.preventDefault(); // Prevent the default hyperlink behavior
-
-  var url = this.getAttribute("href"); // Get the URL from the hyperlink's href attribute
-  var subpage = window.open(url, "_blank", "width=800,height=600,top=100,left=100,resizable=yes"); // Open the subpage in a new floating window
-  subpage.focus(); // Focus on the new window
-}
-
-// Function to attach the click event handler to the "here" hyperlinks
-function attachHyperlinkClickEvent() {
-  var hyperlinks = document.querySelectorAll(".box a");
-  hyperlinks.forEach(function(hyperlink) {
-    hyperlink.addEventListener("click", handleHyperlinkClick);
-  });
-}
-
-// Call the function to attach the click event handler to the hyperlinks
-attachHyperlinkClickEvent();
-
+// Handle navbar toggler icon animation
+$('.navbar-toggler').on('click', function () {
+    $(this).find('.navbar-toggler-icon').toggleClass('active');
+});
